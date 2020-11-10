8fba50bd5369f8c03460f2a376a31a4799cf1033
Artem Ishchenko <proteus11@ukr.net>
2020-11-10 21:27:30 +0200

    filename

--- src/main/java/com/ishchenko/artem/helm/main/handler/controller/HelmController.java
+++ src/main/java/com/ishchenko/artem/helm/main/handler/controller/HelmController.java
@@ -11,11 +11,12 @@ import com.ishchenko.artem.helm.main.parsers.YamlParser;
 import com.ishchenko.artem.helm.main.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -33,16 +34,15 @@ public class HelmController
 
   @RequestMapping(value = "/package/upload", method = RequestMethod.POST)
   @ResponseBody
-  public FlexConfig upload(@RequestBody byte[] body)
+  public FlexConfig upload(@RequestParam("file") MultipartFile body)
       throws IOException
   {
     HelmAttributeParser helmAttributeParser =
         new HelmAttributeParser(new TgzParser(), new YamlParser(), new ProvenanceParser());
     HelmAttributes attributes =
-        helmAttributeParser.getAttributes(AssetKind.HELM_PACKAGE, body);
+        helmAttributeParser.getAttributes(AssetKind.HELM_PACKAGE, body.getBytes());
 
-    //TODO bucket name
-    s3Service.put(bucket, attributes.getName() + "-" + attributes.getVersion(), body);
+    s3Service.put(bucket, body.getOriginalFilename(), body.getBytes());
     return new FlexConfig(attributes.getName(), attributes.getVersion(), null);
   }
 
